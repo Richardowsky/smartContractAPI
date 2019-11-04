b603b9822823863f78dc7e8a70e79b7a597d4387
k.lysenko <konstantin.lysenko@gmail.com>
2019-11-04 16:49:52 +0200

    edit errors

--- src/routes/tokenMarket.js
+++ src/routes/tokenMarket.js
@@ -10,78 +10,62 @@ const marketAddress = "0x3798B9B9b75A8a5AB589B18FaD9Fc7AcD161892C";
 const { abi: abiMarket } = marketJSON;
 const market = new web3.eth.Contract(abiMarket, marketAddress);
 
+router.post('/market', async function (req, res) {
+    const {
+        name,
+        description,
+        symbol,
+        initialSupply,
+        price,
+    } = req.body.token;
 
-router.post('/market', function (req, res) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    const name = req.body.token.name;
-    const description = req.body.token.description;
-    const symbol = req.body.token.symbol;
-    const initialSupply = req.body.token.initialSupply;
-    const price = req.body.token.price;
-
-    market.methods.createProject(name, description, symbol, initialSupply, price).send({from: mainAddress}, function (error, transactionHash) {
+    await market.methods.createProject(name, description, symbol, initialSupply, price).send({from: mainAddress}, function (error, transactionHash) {
         res.end(JSON.stringify("Token - " + name + " created: " + transactionHash));
     });
 });
 
-router.get('/address/:name', function (req, res) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    market.methods.getProject(req.params.name).call((err, result) => {
-        res.end(JSON.stringify("address:" + result));
-    });
+router.get('/address/:name', async function (req, res) {
+    const result = await market.methods.getProject(req.params.name).call();
+    res.end(JSON.stringify("address:" + result));
 });
 
-router.get('/description/:name', function (req, res) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    market.methods.getProjectInfo(req.params.name).call((err, result) => {
-        res.end(JSON.stringify("description:" + result));
-    });
+router.get('/description/:name', async function (req, res) {
+    const result = await market.methods.getProjectInfo(req.params.name).call();
+    res.end(JSON.stringify("description:" + result));
 });
 
-router.get('/price/:name', function (req, res) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    market.methods.getPrice(req.params.name).call((err, result) => {
-        res.end(JSON.stringify("price:" + result));
-    });
+router.get('/price/:name', async function (req, res) {
+    const result = await market.methods.getPrice(req.params.name).call();
+    res.end(JSON.stringify("price:" + result));
 });
 
-router.get('/shares/:name', function (req, res) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    market.methods.getSharesCount(req.params.name).call((err, result) => {
-        res.end(JSON.stringify("shares_amount:" + result));
-    });
+router.get('/shares/:name', async function (req, res) {
+    const result = await market.methods.getSharesCount(req.params.name).call();
+    res.end(JSON.stringify("shares_amount:" + result));
 });
 
-router.post('/v1/market/:name/:amount', function (req, res) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
+router.post('/market/:name/:amount', async function (req, res) {
     const name = req.params.name;
     const amount = req.params.amount;
 
-    market.methods.buyProjectShares(name, amount).send({
+    await market.methods.buyProjectShares(name, amount).send({
         from: mainAddress,
         gasLimit: 550000
-    }, function (error, transactionHash) {
+    }, (error, transactionHash) => {
         res.end(JSON.stringify("Buy - " + name + " token: " + transactionHash));
-        console.log(error);
-        console.log(transactionHash);
     });
-
 });
 
-router.post('/sell/:name/:amount', function (req, res) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
+router.post('/sell/:name/:amount', async function (req, res) {
     const name = req.params.name;
     const amount = req.params.amount;
 
-    market.methods.sellProjectShares(name, amount).send({
+    await market.methods.sellProjectShares(name, amount).send({
         from: mainAddress,
         gasLimit: 550000
-    }, function (error, transactionHash) {
+    }, (error, transactionHash) => {
         res.end(JSON.stringify("Sell - " + name + " token: " + transactionHash));
-        console.log(error);
-        console.log(transactionHash);
     });
-
 });
 
 export default router;
